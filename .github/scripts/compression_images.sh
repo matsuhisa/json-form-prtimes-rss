7e30560d0c3a55359f05781c8d18402e493a0ec1
Matsuhisa Hironobu <matsuhisa+github@gmail.com>
2023-04-11 10:19:21 +0900

    WIP

--- .github/scripts/compression_images.sh
+++ .github/scripts/compression_images.sh
@@ -2,3 +2,5 @@ mv src/tmp/*.jpg src/image
 mogrify -path src/image/ -format jpg src/tmp/*.png
 mogrify src/image/*.jpg -strip -quality 30 -colorspace sRGB
 rm -rf src/tmp
+mkdir src/tmp
+touch src/tmp/.gitkeep
